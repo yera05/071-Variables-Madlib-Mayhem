6367030cb5c424ff046d80e18ab28d8dce0d9b5b
yera05 <43611249+yera05@users.noreply.github.com>
2019-03-05 09:43:50 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,55 @@
+// Declare the following variables and assign appropriate values to them. Make sure your variable names have the same capitalization!
+// Note: "superlative" means the "most" or "best" of something. For example, the "smelliest" or "prettiest."
+// Note: Put quotes, like "smelly", around each of the values (see line 20), except for number1 and number2.
+
+// -  adjective
+var adjective= "cool";
+
+// - animal
+var animal="dog";
+
+// - famousPerson
+var famousperson= "Tfue";
+
+// - food
+var food= "yaroa";
+
+// - liquid
+var liquid="juice";
+
+// - number1
+var number="100";
+
+// - number2
+var number2= "200";
+
+// - pluralCurrency
+var pluralcurrency= "dollar";
+
+// - pluralTypeOfPerson
+var pluraltypeofperson= "lazy";
+
+// - schoolName
+var schoolname= "williams burg art and tech";
+
+// - sport
+var sport= "baseball";
+
+// - subject
+var subject= "math";
+
+// - superlativeAdjective1
+var superlativeAdjective1="biggest";
+
+// - superlativeAdjective2
+var superlativeAsjective2="largest";
+
+// - typeOfVenue
+var typeOfvenue="museo ";
+
+
+
+
+// Now call the fillMadlib function!
+fillMadlib();
+
